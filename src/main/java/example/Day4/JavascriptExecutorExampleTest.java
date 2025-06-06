316dc266990711e0a022eaffbbbfc75aa15f9b18
VHG2k3 <vuhuonggiang1305@gmail.com>
2025-06-06 21:32:05 +0700

    Js

---
+++ src/main/java/example/Day4/JavascriptExecutorExampleTest.java
@@ -0,0 +1,30 @@
+package example.Day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavascriptExecutorExampleTest {
+    static WebDriver driver = null;
+
+    public static void main(String[] args) throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("https://saucelabs.com/request-demo");
+        driver.manage().window().maximize();
+
+        // Lay phan tu DropList (interest) bang cach dung Xpath
+        WebElement in = driver.findElement(By.name("Solution_Interest__c"));
+
+        //Tao doi tuong JJavascriptExecutor
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //Thuc thi
+        js.executeScript("arguments[0].value='Visual Testing';", in);
+        Thread.sleep(3000);
+
+        System.out.println("Selected value from droplist" + in.getAttribute("value"));
+        driver.quit();
+    }
+
+}
